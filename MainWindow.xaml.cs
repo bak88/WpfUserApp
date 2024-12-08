4490d4a820c1c33f35a1c3b18bbe30bec0c25313
bak88 <dimbak1990@gmail.com>
2024-12-08 19:55:33 +0300

    Creat method ButtonRegClick

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace WpfUserApp
         {
             InitializeComponent();
         }
+
+        private void ButtonRegClick(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = passBox.Password.Trim();
+            string passwordRepeat = passBoxRepeat.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введенно не корректно!";
+                textBoxLogin.Background=Brushes.DarkRed;
+            }
+            else if(password.Length < 5) 
+            {
+                passBox.ToolTip = "Это поле введенно не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (password != passwordRepeat)
+            {
+                passBoxRepeat.ToolTip = "Это поле введенно не корректно!";
+                passBoxRepeat.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Это поле введенно не корректно!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = string.Empty;
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = string.Empty;
+                passBox.Background = Brushes.Transparent;
+                passBoxRepeat.ToolTip = string.Empty;
+                passBoxRepeat.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = string.Empty;
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Все хорошо!");
+            }
+
+
+        }
     }
 }
